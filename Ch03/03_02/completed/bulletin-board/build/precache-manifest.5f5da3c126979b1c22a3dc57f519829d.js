3f47be9be9c068be77850ffe84f5775ad5e9fa57
Liinis <47757755+Liinis@users.noreply.github.com>
2019-03-27 14:31:55 +0100

    Klar Bulletin-board app
    klara filer med hela appen inkl övnkoden för kap 3,4,5,6

---
+++ Ch03/03_02/completed/bulletin-board/build/precache-manifest.5f5da3c126979b1c22a3dc57f519829d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d9212d463b78634432f6",
+    "url": "/static/js/main.773b626b.chunk.js"
+  },
+  {
+    "revision": "1e62cec735ffac219a8a",
+    "url": "/static/js/2.da1fd74d.chunk.js"
+  },
+  {
+    "revision": "d9212d463b78634432f6",
+    "url": "/static/css/main.58570c18.chunk.css"
+  },
+  {
+    "revision": "15169e48da45b9301b99c02b6aa3dd51",
+    "url": "/index.html"
+  }
+];
--- Ch03/03_02/completed/bulletin-board/src/index.js
+++ Ch03/03_02/completed/bulletin-board/src/index.js
@@ -1,8 +1,24 @@
+/*gamla 
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Board from './Board';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Board />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();*/
+
+//nya från kap 6
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Board from './Board'
+//import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<Board count={50} />, document.getElementById('root'))
+//registerServiceWorker()
